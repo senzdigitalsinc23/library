afc8cb2d8ffc8794824c38c7bd7efe09f00b4aab
Senz Digitals Inc <senzdigitals.inc23@gmail.com>
2024-05-11 12:48:09 +0000

    Modal For Adding New Books
    Created a dialog modal for creating new books.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,4 @@
-const myLibrary = [];
 
-function Book(title, author, numberOfPages, read = false) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-
-    this.info = () => {
-        return (this.title + " by " + this.author + ", " + this.numberOfPages + " pages, " + (read ? "read" : "not read yet"));
-    }
-}
-
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295');
-const newBook1 = new Book('Atomic Habbits', 'James Clear', '290', true);
-const newBook2 = new Book('The demon of unrest', 'Erik Larson', '295', true);
-const newBook3 = new Book('Say More', 'Jen Psaki', '310');
-const newBook4 = new Book('Indian Cooking', 'Madhur Jaffery', '184', true);
-
-function addBookToLibrary() {
-    myLibrary.push(theHobbit, newBook1, newBook2, newBook3, newBook4);
-}
-
-addBookToLibrary();
-
-//Create elements
 //Helper functions for creating and setting elements on DOM
 function createElements(element, text = "") {
 
@@ -70,6 +45,38 @@ function attachElements(parent, children = "" | []) {
     }
 }
 
+//Methods to add Functionality
+const myLibrary = [];
+
+function Book(title, author, numberOfPages, read = false) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+
+    this.info = () => {
+        return (this.title + " by " + this.author + ", " + this.numberOfPages + " pages, " + (read ? "read" : "not read yet"));
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295');
+const newBook1 = new Book('Atomic Habbits', 'James Clear', '290', true);
+const newBook2 = new Book('The demon of unrest', 'Erik Larson', '295', true);
+const newBook3 = new Book('Say More', 'Jen Psaki', '310');
+const newBook4 = new Book('Indian Cooking', 'Madhur Jaffery', '184', true);
+
+function addBookToLibrary() {
+    myLibrary.push(theHobbit, newBook1, newBook2, newBook3, newBook4);
+}
+
+addBookToLibrary();
+
+function clearModal() {
+    txtBookAuthor.value = "";
+    txtTitle.value = "";
+    numNumberOfPages.value = "";
+}
+
 
 //Create elements and variables
 
@@ -96,75 +103,79 @@ let btnAddBook = createElements('button', "Add Book");
 btnAddBook.classList.add('add-book')
 gridContainer.classList.add('grid-container');
 
-/* setAttributes({
+setAttributes({
     'open':""
-}, dialogModal) */
+}, dialogModal)
 
 setAttributes({'class': 'btn-submit'}, dialogButton)
-setAttributes({'method': 'dialog'}, dialogForm)
-
-attachElements(dialogForm, dialogButton);
-attachElements(container, btnAddBook);
-
-attachElements(dialogModal, [dialogHeader, dialogForm]);
-attachElements(gridContainer, dialogModal);
-attachElements(container, gridContainer);
+setAttributes({'method': 'dialog'}, dialogForm);
 
 let count = 0;
 
-myLibrary.map((book) => {
-    bookCard  = createElements('div');
-    bookCard.classList.add('book-card');
+function loadBooks() {
+    myLibrary.map((book) => {
+        bookCard  = createElements('div');
+        bookCard.classList.add('book-card');
+    
+        author  = createElements('div', book.author);
+        author.classList.add('author');
+    
+        thumbnail = createElements('img');
+        thumbnail.classList.add('thumbnail')
+    
+        setAttributes({'src': "../assets/images/thehobbit.jpg", 'alt': book.title}, thumbnail)
+    
+        bookTitle  = createElements('div', book.title);
+        bookTitle.classList.add('book-title');
+    
+        pages  = createElements('div', book.pages + " pages");
+        pages.classList.add('pages');
+    
+        bookRead  = createElements('div', "Status: " + (book.read ? "Read" : "Not read yet"));
+        bookRead.classList.add('status');
+    
+        author = createElements('h3', "By: " + book.author);
+    
+        pages = createElements('h4', book.numberOfPages + " pages.")
+    
+        setAttributes({'style':"margin: 6px 10px;"}, [author, pages])
+    
+        let thumbnailContainer = createElements("div");
+        thumbnailContainer.classList.add("thumbnail-container");
+    
+        let deleteImg = createElements('img');
+        setAttributes({'style': "width: 15px", 'src': "../assets/images/delete-red.png"}, deleteImg)
+    
+        let btnRemoveBook = createElements('button');
+        setAttributes({'class': "btn-remove", 'data-btn-remove': "book-" + count + "-remove"}, btnRemoveBook)
+    
+        let btnStatus = createElements('button', 'Not Read');
+        setAttributes({'class': "btn-status",'data-btn-status': "book-" + count + "-status"}, btnStatus);
+    
+        attachElements(btnRemoveBook, deleteImg);
+    
+        attachElements(thumbnailContainer, thumbnail)
+        attachElements(bookCard, [bookTitle, thumbnailContainer, author, pages, btnStatus, btnRemoveBook]);
+        attachElements(gridContainer, bookCard);
+    
+        count++;
+    
+    })
+    
+    attachElements(container, btnAddBook);
+    
+    attachElements(dialogForm, [dialogHeader, txtTitle, txtBookAuthor, numNumberOfPages,dialogButton])
+    attachElements(dialogModal, dialogForm);
+    attachElements(gridContainer, dialogModal);
+    attachElements(container, gridContainer);
+}
 
-    author  = createElements('div', book.author);
-    author.classList.add('author');
-
-    thumbnail = createElements('img');
-    thumbnail.classList.add('thumbnail')
-
-    setAttributes({'src': "../assets/images/thehobbit.jpg"}, thumbnail)
-
-    bookTitle  = createElements('div', book.title);
-    bookTitle.classList.add('book-title');
-
-    pages  = createElements('div', book.pages + " pages");
-    pages.classList.add('pages');
-
-    bookRead  = createElements('div', "Status: " + (book.read ? "Read" : "Not read yet"));
-    bookRead.classList.add('status');
-
-    author = createElements('h3', "By: " + book.author);
-
-    pages = createElements('h4', book.numberOfPages + " pages.")
-
-    setAttributes({'style':"margin: 6px 10px;"}, [author, pages])
-
-    let thumbnailContainer = createElements("div");
-    thumbnailContainer.classList.add("thumbnail-container");
-
-    let deleteImg = createElements('img');
-    setAttributes({'style': "width: 15px", 'src': "../assets/images/delete-red.png"}, deleteImg)
-
-    let btnRemoveBook = createElements('button');
-    setAttributes({'class': "btn-remove", 'data-btn-remove': "book-" + count + "-remove"}, btnRemoveBook)
-
-    let btnStatus = createElements('button', 'Not Read');
-    setAttributes({'class': "btn-status",'data-btn-status': "book-" + count + "-status"}, btnStatus);
-
-    attachElements(btnRemoveBook, deleteImg);
-
-    attachElements(thumbnailContainer, thumbnail)
-    attachElements(bookCard, [bookTitle, thumbnailContainer, author, pages, btnStatus, btnRemoveBook]);
-    attachElements(gridContainer, bookCard);
-
-    count++;
-
-})
-
-
+loadBooks();
 
 //Events
 dialogButton.onclick = (e) => {
+    clearModal();
+
     dialogModal.close();
     e.preventDefault();
 }
@@ -172,3 +183,4 @@ dialogButton.onclick = (e) => {
 btnAddBook.onclick = () => {
     dialogModal.showModal();
 }
+
